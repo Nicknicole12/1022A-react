d161f55b380d33738b00d3e34d4343a10d2fa731
Nicknicole12 <nicole.gois@estudante.ifms.edu.br>
2024-03-12 10:05:11 -0400

    Meu Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MeuComponente from'./componentes/MeuComponente'
+
+function App() {
+ let nome = "TERE"
+ let sobrenome = "Terenciani"
+
+  return (<div>
+   <h1 className="meu-nome">OLA EU SOU O {nome} {sobrenome}</h1>
+  <h1>OLA EU SOU O {nome} {sobrenome}</h1>
+  </div>
+  )
+}
+
+export default App
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,14 @@
+//Faça um componente que faça um retângulo da largura da tela por 200px
+// crie uma variavel com LET no js que contenha o valor  "Meu Componente"
+// crie uma div no componente que dentro dela mostre um <p>
+// contendo o valor da variavel.
+import './MeuComponente.css'
+Function MeuComponente(){
+    let variavel = "Meu Componente"
+    return(
+        <div className="minha-div">
+            <p>{variavel}</p>
+        </div>
+    )
+}
+export default MeuComponente;
